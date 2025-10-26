dc4ccc73ff752ba463b95fe10cfc1a1c68c0414f
AlexDvor <alexmatvichuk@gmail.com>
2025-10-26 17:19:37 +0100

    ref

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,12 +1,33 @@
-export const createImgCard = ({ webformatURL, largeImageURL, tags }) => {
+export const createImgCard = ({
+  downloads = 0,
+  comments = 0,
+  likes = 0,
+  views = 0,
+  webformatURL,
+  largeImageURL,
+  tags,
+}) => {
   return `<li class="gallery-item">
   <a class="gallery-link" href=${largeImageURL}>
-    <img
-      class="gallery-image"
-      src=${webformatURL}
-      data-source=${largeImageURL}
-      alt=${tags}
-    />
+    <img class="gallery-image" src=${webformatURL} data-source=${largeImageURL} alt=${tags} />
+    <div class='wrapper-info'>
+      <div class='info'>
+        <p>Likes</p>
+        <p>${likes}</p>
+      </div>
+      <div class='info'>
+        <p>Views</p>
+        <p>${views}</p>
+      </div>
+      <div class='info'>
+        <p>Coments</p>
+        <p>${comments}</p>
+      </div>
+      <div class='info'>
+        <p>Downloads</p>
+        <p>${downloads}</p>
+      </div>
+    </div>
   </a>
 </li>
 `;
--- src/main.js
+++ src/main.js
@@ -1,20 +1,58 @@
 import SimpleLightbox from 'simplelightbox';
 import { API_PixaBay } from './js/pixabay-api';
 import { createImgCard } from './js/render-functions';
+import iziToast from 'izitoast';
 
 const refs = {
   form: document.querySelector('.form'),
   imgWrap: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader'),
+};
+
+const messageForUser = (ms, type) => {
+  if (type === 'error') {
+    iziToast.error({
+      message: ms,
+      position: 'topRight',
+    });
+  } else if (type === 'warning') {
+    iziToast.warning({
+      message: ms,
+      position: 'topRight',
+    });
+  } else {
+    iziToast.show({
+      message: ms,
+      position: 'topRight',
+    });
+  }
 };
 
 const onSubmitBtn = e => {
   e.preventDefault();
   const form = e.target;
   const formData = new FormData(form);
-  const searchField = formData.get('search-field');
+  const searchField = formData.get('search-field').trim();
+
+  if (!searchField) {
+    messageForUser(
+      'The search field is empty. Please enter text to search.',
+      'warning'
+    );
+    return;
+  }
+
+  refs.loader.classList.add('is-loaded');
 
   API_PixaBay.getPhotoByQuery(searchField)
     .then(data => {
+      if (!data.hits?.length) {
+        messageForUser(
+          'Sorry, there are no images matching your search query. Please try again!',
+          'error'
+        );
+      }
+      console.log(data.hits);
       const createGallery = data.hits.map(item => createImgCard(item)).join('');
 
       refs.imgWrap.innerHTML = createGallery;
@@ -24,7 +62,10 @@ const onSubmitBtn = e => {
         overlayOpacity: 0.9,
       });
     })
-    .catch(e => console.log(e));
+    .catch(e => console.log(e))
+    .finally(() => {
+      refs.loader.classList.remove('is-loaded');
+    });
 };
 
 refs.form.addEventListener('submit', onSubmitBtn);

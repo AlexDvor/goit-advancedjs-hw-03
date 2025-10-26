066697f45272b55cb03190c842030d237c947f0a
AlexDvor <alexmatvichuk@gmail.com>
2025-10-26 15:48:55 +0100

    Add Slider

--- src/main.js
+++ src/main.js
@@ -3,28 +3,28 @@ import { API_PixaBay } from './js/pixabay-api';
 import { createImgCard } from './js/render-functions';
 
 const refs = {
+  form: document.querySelector('.form'),
   imgWrap: document.querySelector('.gallery'),
 };
 
-// const createGallery = images.map(item => createImgCard(item)).join('');
-// refs.imgWrap.innerHTML = createGallery;
-// new SimpleLightbox('.gallery-link', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-//   overlayOpacity: 0.9,
-// });
+const onSubmitBtn = e => {
+  e.preventDefault();
+  const form = e.target;
+  const formData = new FormData(form);
+  const searchField = formData.get('search-field');
 
-API_PixaBay.getPhotoByQuery('cat')
-  .then(data => {
-    const createGallery = data.hits.map(item => createImgCard(item)).join('');
+  API_PixaBay.getPhotoByQuery(searchField)
+    .then(data => {
+      const createGallery = data.hits.map(item => createImgCard(item)).join('');
 
-    refs.imgWrap.innerHTML = createGallery;
-    new SimpleLightbox('.gallery-link', {
-      captionsData: 'alt',
-      captionDelay: 250,
-      overlayOpacity: 0.9,
-    });
-  })
-  .catch(e => console.log(e));
+      refs.imgWrap.innerHTML = createGallery;
+      new SimpleLightbox('.gallery-link', {
+        captionsData: 'alt',
+        captionDelay: 250,
+        overlayOpacity: 0.9,
+      });
+    })
+    .catch(e => console.log(e));
+};
 
-// largeImageURL;
+refs.form.addEventListener('submit', onSubmitBtn);
